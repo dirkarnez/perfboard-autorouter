15bdbead7f3cf833dd5585323efe786ad1bb3e14
Dirk Arnez <smalldirkalex@gmail.com>
2023-12-04 23:52:59 +0800

    - update app.ts

--- app.ts
+++ app.ts
@@ -24,6 +24,10 @@ namespace DataStructure {
       return this.edges;
     }
 
+    getName() {
+      return this.name;
+    }
+
     getValue() {
       return this.value;
     }
@@ -32,12 +36,10 @@ namespace DataStructure {
   export class Graph<T> {
     addEdge(fromNode: Node<T>, toNode: Node<T>) {
       fromNode.addEdge(toNode);
-      toNode.addEdge(fromNode);
     }
 
     depthFirstTraversalRecursively(startNode: Node<T>, visited: Set<Node<T>> = new Set<Node<T>>()) {
       visited.add(startNode);
-      console.log(startNode.getValue());
 
       for (const edge of startNode.getEdges()) {
         if (!visited.has(edge)) {
@@ -46,7 +48,7 @@ namespace DataStructure {
       }
     }
     
-    depthFirstTraversalIteratively(startNode: Node<T>) {
+    depthFirstTraversalIteratively(startNode: Node<T>, callback: (current: Node<T>, hasEdges: boolean) => void) {
       const visited = new Set<Node<T>>();
       const stack: Node<T>[] = [];
 
@@ -56,13 +58,15 @@ namespace DataStructure {
         const currentNode = stack.pop();
         if (currentNode && !visited.has(currentNode)) {
           visited.add(currentNode);
-          console.log(currentNode.getValue());
-
-          for (const edge of currentNode.getEdges()) {
-            if (!visited.has(edge)) {
-              stack.push(edge);
+    
+          //console.log(currentNode.getValue());
+          const edges = currentNode.getEdges();
+            for (const edge of edges) {
+                //if (!visited.has(edge)) {
+                    stack.push(edge);
+                //}
             }
-          }
+            callback(currentNode, (edges.length > 0));
         }
       }
     }
@@ -70,17 +74,26 @@ namespace DataStructure {
 } 
 
 namespace NetList {
+    class IDAssigner {
+        private count: number = 0;
+        get ID() {
+            return this.count++;
+        }
+    }
+
   abstract class Component extends DataStructure.Node<string> {};
 
   export class LM741 extends Component {
+    static idAssigner = new IDAssigner();
     constructor () {
-      super("LM741", "LM741", 6);
+      super(`LM741#${LM741.idAssigner.ID}`, "LM741", 6);
     }
   }
 
   export class Wire extends Component {
+    static idAssigner = new IDAssigner();
     constructor () {
-      super("Wire", "Wire", 2);
+      super(`Wire#${Wire.idAssigner.ID}`, "Wire", 2);
     }
   }
 
@@ -97,12 +110,25 @@ namespace NetList {
       this.graph = new DataStructure.Graph<string>();
     }
 
-    addComponent(from: Component, to: Component) {
-      from.addEdge(to);
+    addComponents(from: Component, ...to: Component[]) {
+      from.addEdge(to[0]);
+      
+      for (var i = 0; i < to.length - 1; i++)
+      {
+        let _from = to[i];
+        let _to = to[i+1];
+        _from.addEdge(_to);
+      }
     }
 
-    depthFirstTraversalIteratively(from: Component) {
-      this.graph.depthFirstTraversalIteratively(from);
+    depthFirstTraversalIteratively(from: Component): string{
+        var netlistfile: string = `${from.getName()}\n`;
+
+        this.graph.depthFirstTraversalIteratively(from, (currentNode, hasEdges) => {
+            netlistfile += `${currentNode != from ? `${currentNode.getName()}${hasEdges ? "->" : "\n"}` : ""}`;
+        });
+        
+        return netlistfile;
     }
 
     depthFirstTraversalRecursively(from: Component) {
@@ -122,7 +148,16 @@ namespace NetList {
 
 const circuit = new NetList.Circuit();
 const tts = new NetList.ThreeTerminalSocket();
-circuit.addComponent(tts, new NetList.LM741());
+
+const lm741 = new NetList.LM741();
+console.log(lm741.getName());
+
+circuit.addComponents(tts, new NetList.Wire(), lm741);
+
+circuit.addComponents(lm741, new NetList.Wire(), lm741);
+
+
+
 // const edges = node1.getEdges();
 
 // if (edges) {
@@ -132,5 +167,4 @@ circuit.addComponent(tts, new NetList.LM741());
 // }
 
 //graph.depthFirstTraversalRecursively(node1);
-circuit.depthFirstTraversalRecursively(tts);
-
+console.log(circuit.depthFirstTraversalIteratively(tts));
